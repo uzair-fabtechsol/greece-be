61919c7b7727dc5efde30dfcedbda50bb9efd234
Muh-Uzair <muhammaduzair1062001@gmail.com>
2026-07-21 14:31:26 +0500

    initial setup done

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,7 +1,18 @@
 import mongoose from "mongoose";
 import { env } from "@src/config/env";
 
-export const connectDB = async (): Promise<void> => {
-  await mongoose.connect(env.MONGO_DB_CONNECTION_STRING);
-  console.log("Database connected successfully");
+mongoose.plugin((schema) => {
+  schema.set("versionKey", false);
+});
+
+export const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(env.MONGO_DB_CONNECTION_STRING, {
+      family: 4,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    process.exit(1); // Exit process with failure
+  }
 };
